5f0508d0172a8608abd785a04a19978d50ab67f1
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-01-12 19:07:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
 function convertToRoman(num) {
-  	const lookupTable = {
-      M:1000, 
-      CM:900, 
-      D:500, 
-      CD:400, 
-      C:100, 
-      CX:90, 
-      L:50, 
-      LX:40, 
-      X:10, 
-      IX:9, 
-      V:5, 
-      IV:4, 
-      I:1
-    };
+//   	const lookupTable = {
+//       M:1000, 
+//       CM:900, 
+//       D:500, 
+//       CD:400, 
+//       C:100, 
+//       CX:90, 
+//       L:50, 
+//       LX:40, 
+//       X:10, 
+//       IX:9, 
+//       V:5, 
+//       IV:4, 
+//       I:1
+//     };
 
-  //your code here
-	let accumulator='';
+//   //your code here
+// 	let accumulator='';
 
 	
-for(const key in lookupTable){
-const numberValue=lookupTable[key];
-while(numberValue<=num){
- num-=numberValue;
- accumulator+=key;
+// for(const key in lookupTable){
+// const numberValue=lookupTable[key];
+// while(numberValue<=num){
+//  num-=numberValue;
+//  accumulator+=key;
 
-}
+// }
 	
-}
-return accumulator;
+// }
+// return accumulator;
+  const map = {
+    M:  1000,
+    CM: 900,
+    D:  500,
+    CD: 400,
+    C:  100,
+    XC: 90,
+    L:  50,
+    XL: 40,
+    X:  10,
+    IX: 9,
+    V:  5,
+    IV: 4,
+    I:  1,
+  };  
+  let result = '';
+  
+  for (key in map) {
+    result += key.repeat(Math.floor(num / map[key]));
+    num %= map[key];
+  }
+  
+  return result;
+	
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
